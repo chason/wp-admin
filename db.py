fdd8ecf3c499d5235aebef9d1a9229b44ded99cb
Chason Chaffin <chason@gmail.com>
2011-06-06 22:20:16 -0700

    Added tests

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import MySQLdb
 import re
 
 # Methods for reading database information from WordPress and connecting to MySQL
@@ -11,11 +12,20 @@ def get_details(location):
     except IOError as e:
         print("({0})".format(e))
     
-    pattern = re.compile('''define\('(?P<name>[A-Za-z_0-9]+)',\s?'(?P<value>[^\']+)'\);''')
+    pattern = re.compile('''define\('(?P<name>[A-Za-z_0-9]+)',\s?'(?P<value>[^\']*)'\);''')
+    pattern2 = re.compile('''\$table_prefix\s*=\s*'(?P<value>[^\']*)';''')
     for line in wpconfig:
         match = pattern.search(line)
         if match:
             details.update({match.group('name'): match.group('value')})
+        else:
+            match = pattern2.search(line)
+            if match:
+                details.update({'table_prefix': match.group('value')})
     wpconfig.close()
     return details
 
+def connect(database, dbuser, dbpass):
+    db = MySQLdb.connect(user=dbuser, passwd=dbpass)
+    c = db.cursor()
+    
--- test.py
+++ test.py
@@ -3,4 +3,21 @@
 import db
 import unittest
 
+class TestDB(unittest.TestCase):
+    
+    sample_details = {
+            'DB_NAME': 'testname',
+            'DB_USER': 'testuser',
+            'DB_PASSWORD': 'testpass',
+            'DB_HOST': 'testhost',
+            'DB_CHARSET': 'utf8',
+            'DB_COLLATE': '',
+            'table_prefix': 'wp_',
+        }
 
+    def test_get_details(self):
+        details = db.get_details('.')
+        self.failUnlessEqual(details,self.sample_details)  
+
+if __name__ == '__main__':
+    unittest.main()
