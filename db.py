ac5eb7e2d1b7846d96e9f05c907c8d0bb2353461
Chason Chaffin <chason@gmail.com>
2011-06-14 22:57:40 -0700

    Can add and subtract admin users from db now

--- db.py
+++ db.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
+import random
+import string
 import MySQLdb
 import re
+import time
 
 # Methods for reading database information from WordPress and connecting to MySQL
 
@@ -25,7 +28,86 @@ def get_details(location):
     wpconfig.close()
     return details
 
-def connect(database, dbuser, dbpass):
-    db = MySQLdb.connect(user=dbuser, passwd=dbpass)
+def dbconnect(details):
+    db = MySQLdb.connect(user=details['DB_USER'], passwd=details['DB_PASSWORD'], host=details['DB_HOST'], db=details['DB_NAME'])
     c = db.cursor()
-    
+    return c    
+
+def genpass(digits=8, chars=string.letters + string.digits):
+    return ''.join([random.choice(chars) for i in xrange(digits)])
+
+def create_admin(db, details, **kargs): 
+    user = {
+        'name': 'tempadmin',
+        'password': genpass(8),
+        'nicename': 'tempadmin',
+        'email': 'nobody@domain.com',
+    }
+    for key in kargs.keys():
+        user.update({key: kargs[key]})
+        
+    users_sql = """
+    INSERT INTO {prefix}users SET 
+        user_login='{name}',
+        user_pass=md5('{password}'), 
+        user_nicename='{nicename}',
+        user_email='{email}', 
+        user_registered='{date}', 
+        user_status='0',
+        display_name='{nicename}';
+    """.format(prefix=details['table_prefix'], name=user['name'],
+            password=user['password'], nicename=user['nicename'], 
+            email=user['email'], date=time.strftime("%Y-%m-%d %H:%M:%S"))
+
+    users_sql2 = """
+    SELECT ID from {prefix}users WHERE user_login='{name}';
+    """.format(prefix=details['table_prefix'], name=user['name'])
+
+    db.execute(users_sql)
+    db.execute(users_sql2)
+    uid, = db.fetchone()
+    uid = int(uid)
+
+    meta_sql = """
+    INSERT INTO {prefix}usermeta SET
+        user_id='{uid}',
+        meta_key='wp_capabilities',
+        meta_value='a:1:{{s:13:"administrator";b:1;}};'
+    """.format(prefix=details['table_prefix'], uid=uid)
+
+    meta_sql2 = """
+    INSERT INTO {prefix}usermeta SET
+        user_id='{uid}',
+        meta_key='wp_user_level',
+        meta_value='10';
+    """.format(prefix=details['table_prefix'], uid=uid)
+
+    db.execute(meta_sql)
+    db.execute(meta_sql2)
+
+    user.update({'uid': uid})
+
+    return user
+
+def delete_user(db, details, username):
+    uid_sql = """
+    SELECT ID from {prefix}users WHERE user_login='{name}';
+    """.format(prefix=details['table_prefix'], name=username)
+    db.execute(uid_sql)
+    uid, = db.fetchone()
+    uid = int(uid)
+
+    users_sql = """
+    DELETE FROM {prefix}users WHERE ID={uid};
+    """.format(prefix=details['table_prefix'], uid=uid)
+    db.execute(users_sql)
+
+    meta_sql = """
+    delete FROM {prefix}usermeta WHERE user_id={uid};
+    """.format(prefix=details['table_prefix'], uid=uid)
+    db.execute(meta_sql)
+
+    some_left = db.execute(uid_sql)
+    if some_left:
+        delete_user(db, details, username)
+
