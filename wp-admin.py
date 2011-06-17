a926cf4a1597cb5c06d86e242678c1819b0b1557
Chason Chaffin <chason@gmail.com>
2011-06-16 20:57:40 -0700

    Changed formatting of output

--- wp-admin.py
+++ wp-admin.py
@@ -88,13 +88,13 @@ class WPAdmin(object):
         @returns: 0
         """
         print("""
-            User successfully created:
-            Username: {name}
-            Password: {passwd}
-            Nicename: {name}
-            Email: {email}
-            """.format(name=user['name'], passwd=user['password'], 
-                    email=user['email']))
+        User successfully created:
+        Username: {name}
+        Password: {passwd}
+        Nicename: {name}
+        Email: {email}
+        """.format(name=user['name'], passwd=user['password'], 
+                   email=user['email']))
         
         return 0
 
@@ -225,7 +225,7 @@ class WPAdmin(object):
             self.print_user_details(user)
         if self.options.del_user:
             self.delete_user(db, details, self.options.user)
-            print("{0} successfully deleted.\n".format(self.options.user))
+            print("All users with username '{0}' successfully deleted.".format(self.options.user))
         
 def setup_parser():
     """
